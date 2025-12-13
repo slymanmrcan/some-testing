36d2efd19639499afd3428e52f811c1ff5ef0e5d
sanan <34422966+slymanmrcan@users.noreply.github.com>
2025-12-13 17:07:28 +0300

    Update controls.js

--- 3d/engine/controls.js
+++ 3d/engine/controls.js
@@ -1,1 +1,13 @@
 
+import { PointerLockControls }
+  from "https://unpkg.com/three@0.160.0/examples/jsm/controls/PointerLockControls.js";
+
+export function initControls(camera, dom) {
+  const controls = new PointerLockControls(camera, dom);
+
+  document.addEventListener("click", () => {
+    controls.lock();
+  });
+
+  return controls;
+}
