af50f9cc314695760bc6e65536a0aa1c5b035c44
sanan <34422966+slymanmrcan@users.noreply.github.com>
2025-12-13 17:07:04 +0300

    Update main.js

--- 3d/main.js
+++ 3d/main.js
@@ -1,1 +1,16 @@
 
+import { initScene } from "./engine/scene.js";
+import { initCamera } from "./engine/camera.js";
+import { initControls } from "./engine/controls.js";
+import { createRoom } from "./world/room.js";
+import { createProjects } from "./world/projects.js";
+import { startLoop } from "./engine/loop.js";
+
+const { scene, renderer } = initScene();
+const camera = initCamera();
+const controls = initControls(camera, renderer.domElement);
+
+createRoom(scene);
+const projects = createProjects(scene);
+
+startLoop({ scene, camera, renderer, controls, projects });
