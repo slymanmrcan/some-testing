f076f563594e614db949b1e10b403cab6ed6c9a1
sanan <34422966+slymanmrcan@users.noreply.github.com>
2025-12-13 17:07:54 +0300

    Update loop.js

--- 3d/engine/loop.js
+++ 3d/engine/loop.js
@@ -1,1 +1,28 @@
 
+import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
+
+const raycaster = new THREE.Raycaster();
+const hud = document.getElementById("project");
+
+export function startLoop({ scene, camera, renderer, controls, projects }) {
+  function animate() {
+    requestAnimationFrame(animate);
+
+    raycaster.setFromCamera(new THREE.Vector2(0,0), camera);
+    const hits = raycaster.intersectObjects(projects);
+
+    if (hits.length) {
+      hud.style.display = "block";
+      hud.innerHTML = `
+        <strong>${hits[0].object.userData.name}</strong><br>
+        ${hits[0].object.userData.desc}
+      `;
+    } else {
+      hud.style.display = "none";
+    }
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
+}
